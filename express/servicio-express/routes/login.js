acc3867f316f425c14a544f8e283ea6f9faef500
ramsesduidar <neovongolaprimo1@outlook.com>
2024-04-29 19:41:55 +0200

    login form v2

--- express/servicio-express/routes/login.js
+++ express/servicio-express/routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('login', { title: 'CityBike-Login' });
 });
 
 module.exports = router;
