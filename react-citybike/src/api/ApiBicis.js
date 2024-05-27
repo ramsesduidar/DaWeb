af81684f6c799469ca70193ef61e8026d2a5aaaa
edu8000 <educraft8000@gmail.com>
2024-05-27 19:06:49 +0200

    Primer commit UsuarioApp

--- react-citybike/src/api/ApiBicis.js
+++ react-citybike/src/api/ApiBicis.js
@@ -98,3 +98,241 @@ export async function darBajaBici(idBici, motivo){
             throw new Error(error.message)
         })
 }
+
+async function checkActive(usuarioId) {
+    const token = getToken();
+    if (!token) {
+        throw new Error('Token no encontrado en localStorage');
+    }
+
+    let req = new Request(`http://localhost:8090/alquileres/usuarios/${usuarioId}`, {
+        method: 'GET',
+        redirect: 'follow',
+        headers: new Headers({
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        })
+    });
+
+    return fetch(req)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error al obtener datos del usuario");
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Check if the user has any active rentals or reservations
+            const hasActiveRentOrReserve = data.alquileres.length > 0 || data.reservas.length > 0;
+            return hasActiveRentOrReserve;
+        })
+        .catch(error => {
+            console.error(error);
+            throw new Error(error.message);
+        });
+}
+
+export async function alquilarBici(usuarioId, idBici) {
+
+  try {
+
+    const hasActiveRentOrReserve = await checkActive(usuarioId);
+        if (hasActiveRentOrReserve) {
+            throw new Error("El usuario ya tiene una reserva o alquiler activo");
+        }
+
+  const token = getToken();
+  if (!token) {
+      throw new Error('Token no encontrado en localStorage');
+  }
+
+  const alquiler = {
+      idBicicleta: idBici,
+      inicio: new Date().toISOString(),
+      fin: null
+  };
+
+  const rentData = {
+      id: usuarioId,
+      reservas: [],
+      alquileres: [alquiler]
+  };
+
+  let req = new Request(`http://localhost:8090/alquileres/usuarios/${usuarioId}`, {
+      method: 'POST',
+      redirect: 'follow',
+      headers: new Headers({
+          "Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json"
+      }),
+      body: JSON.stringify(rentData)
+  });
+
+  return fetch(req)
+      .then(response => {
+          console.log("respuesta rent bici: " + response.ok);
+
+          if (!response.ok) {
+              throw new Error("Error inesperado al alquilar la bici, intentelo de nuevo");
+          }
+
+          return response.status;
+      })
+      .catch(error => {
+          console.log(error);
+          throw new Error(error.message);
+      });
+    } catch(error) {
+        console.error(error.message);
+        throw error;
+    }
+}
+
+export async function reservarBici(usuarioId, idBici) {
+
+try {
+
+    const hasActiveRentOrReserve = await checkActive(usuarioId);
+    if (hasActiveRentOrReserve) {
+        throw new Error("El usuario ya tiene una reserva o alquiler activo");
+    }
+  const token = getToken();
+  if (!token) {
+      throw new Error('Token no encontrado en localStorage');
+  }
+
+  const ahora = new Date();
+  const caducidad = new Date(ahora.getTime() + 30 * 60000); // 30 minutos
+
+  const reserva = {
+      idBicicleta: idBici,
+      creada: ahora.toISOString(),
+      caducidad: caducidad.toISOString()
+  };
+
+  const reservaData = {
+      id: usuarioId,
+      reservas: [reserva],
+      alquileres: []
+  };
+
+  let req = new Request(`http://localhost:8090/alquileres/usuarios/${usuarioId}`, {
+      method: 'POST',
+      redirect: 'follow',
+      headers: new Headers({
+          "Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json"
+      }),
+      body: JSON.stringify(reservaData)
+  });
+
+  return fetch(req)
+      .then(response => {
+          console.log("respuesta reserve bici: " + response.ok);
+
+          if (!response.ok) {
+              throw new Error("Error inesperado al reservar la bici, intentelo de nuevo");
+          }
+
+          return response.status;
+      })
+      .catch(error => {
+          console.log(error);
+          throw new Error(error.message);
+      });
+    } catch(error) {
+        console.error(error.message);
+        throw error;
+    }
+}
+
+export async function confirmarReserva(usuarioId, idBici) {
+  const token = getToken();
+  if (!token) {
+      throw new Error('Token no encontrado en localStorage');
+  }
+
+  const ahora = new Date();
+  const alquiler = {
+      idBicicleta: idBici,
+      inicio: ahora.toISOString(),
+      fin: null
+  };
+
+  const confirmData = {
+      id: usuarioId,
+      reservas: [],
+      alquileres: [alquiler]
+  };
+
+  let req = new Request(`http://localhost:8090/alquileres/usuarios/${usuarioId}`, {
+      method: 'POST',
+      redirect: 'follow',
+      headers: new Headers({
+          "Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json"
+      }),
+      body: JSON.stringify(confirmData)
+  });
+
+  return fetch(req)
+      .then(response => {
+          console.log("respuesta confirmar reserva: " + response.ok);
+
+          if (!response.ok) {
+              throw new Error("Error inesperado al confirmar la reserva, intentelo de nuevo");
+          }
+
+          return response.status;
+      })
+      .catch(error => {
+          console.log(error);
+          throw new Error(error.message);
+      });
+}
+
+export async function endAlquiler(usuarioId, idBici) {
+  const token = getToken();
+  if (!token) {
+      throw new Error('Token no encontrado en localStorage');
+  }
+
+  const ahora = new Date().toISOString();
+
+  const endData = {
+      id: usuarioId,
+      reservas: [],
+      alquileres: [
+          {
+              idBicicleta: idBici,
+              inicio: null,
+              fin: ahora
+          }
+      ]
+  };
+
+  let req = new Request(`http://localhost:8090/alquileres/usuarios/${usuarioId}`, {
+      method: 'PATCH',
+      redirect: 'follow',
+      headers: new Headers({
+          "Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json"
+      }),
+      body: JSON.stringify(endData)
+  });
+
+  return fetch(req)
+      .then(response => {
+          console.log("respuesta terminar alquiler: " + response.ok);
+
+          if (!response.ok) {
+              throw new Error("Error inesperado al terminar el alquiler, intentelo de nuevo");
+          }
+
+          return response.status;
+      })
+      .catch(error => {
+          console.log(error);
+          throw new Error(error.message);
+      });
+}
--- react-citybike/src/usuario/UsuarioApp.js
+++ react-citybike/src/usuario/UsuarioApp.js
@@ -1,8 +1,60 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './UsuarioApp.css'
+
+import Bicis from '../gestor/Bicis';
+import NavbarGestor from '../navbar/NavbarGestor';
+import Footer from '../footer/Footer';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Header } from '../header/Header';
+import PageVerEstacionGestor from '../gestor/PageVerEstacionGestor';
+import PageEstacionesGestor from '../gestor/PageEstacionesGestor';
 
 function UsuarioApp() {
     return (
-      <h1>Este es el contenido del usuario</h1>
-      
+      <Container fluid className='contenedor-flex'>
+      <Row>
+        <Col className='no-padding'>
+          <Header></Header>
+        </Col>
+      </Row>
+      <Row className='main'>
+        <Col xs={12} md={2} className='columna-flex no-padding'>
+          <NavbarGestor />
+        </Col>
+        <Col xs={12} md={10} className='no-padding'>
+          <main>
+              <Switch>
+                <Route exact path="/">
+                  <h2>Contenido main gestor</h2>
+                </Route>
+                <Route path="/estaciones/:id">
+                  <PageVerEstacionGestor/>
+                </Route>
+                <Route path="/estaciones">
+                  <PageEstacionesGestor />
+                </Route>
+                <Route path="/bicis">
+                  <Bicis />
+                </Route>
+                <Route path="*">
+                  <h1>Not Found</h1>
+                </Route>
+              </Switch>
+          </main>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='no-padding'>
+          <Footer/>
+        </Col>
+      </Row>
+    </Container>
     );
 }
   
